814a47bc2ca41fab6ff0eca736a2ea315be23341
yongjae213 <y4im@ucsd.edu>
2022-02-17 18:42:51 -0800

    First commit for Calculator project

---
+++ calculatorLogic.py
@@ -0,0 +1,20 @@
+def addition():
+
+    val1 = float(input("Enter first input: "))
+    val2 = float(input("Enter second input: "))
+    print(val1 + val2)
+
+def subtraction():
+    val1 = float(input("Enter first input: "))
+    val2 = float(input("Enter second input: "))
+    print(val1 - val2)
+
+def multiplication():
+    val1 = float(input("Enter first input: "))
+    val2 = float(input("Enter second input: "))
+    print(val1 * val2)
+
+def division():
+    val1 = float(input("Enter first input: "))
+    val2 = float(input("Enter second input: "))
+    print(val1 / val2)
---
+++ main.py
@@ -0,0 +1,26 @@
+from calculatorLogic import *
+
+def main():
+    print("Choose which operation to run: ")
+    print("'1' for addition")
+    print("'2' for subtraction")
+    print("'3' for multiplication")
+    print("'4' for division")
+    while True:
+        switch = input("Please type your operation: ")
+        if switch == '1':
+            addition()
+            break
+        elif switch == '2':
+            subtraction()
+            break
+        elif switch == '3':
+            multiplication()
+            break
+        elif switch == '4':
+            division()
+            break
+        else:
+            print('Error. Please type the correct operation.')
+            continue
+main()
